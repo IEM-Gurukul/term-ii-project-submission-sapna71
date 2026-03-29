cf8d9850124d0187d40c0cf7354d15bc2c28c184
Sapna Singh <sapnasinghh71@gmail.com>
2026-03-29 21:46:58 +0530

    Updated DoorOpenState

--- src/DoorOpenState.java
+++ src/DoorOpenState.java
@@ -1,6 +1,7 @@
 public class DoorOpenState implements State {
     public void handleRequest(Elevator elevator, int floor) {
         System.out.println("Door opening at floor " + elevator.getCurrentFloor());
+        // Reset the elevator to Idle so it can accept the next request in the queue
         elevator.setState(new IdleState());
     }
 }
