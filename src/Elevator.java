8b5233ca123694b0bd9ef04d24745413211c1192
Sapna <sapnasinghh71@gmail.com>
2026-03-29 17:23:05 +0530

    Implemented Queue for handling multiple elevator requests

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,18 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Elevator {
+
     private State currentState;
     private int currentFloor;
+    private Queue<Integer> requests;
 
     public Elevator() {
         currentState = new IdleState();
         currentFloor = 0;
+        requests = new LinkedList<>();
     }
 
     public void setState(State state) {
         this.currentState = state;
-    }
-
-    public void requestFloor(int floor) {
-        currentState.handleRequest(this, floor);
     }
 
     public int getCurrentFloor() {
@@ -21,5 +23,20 @@ public class Elevator {
 
     public void setCurrentFloor(int floor) {
         this.currentFloor = floor;
+    }
+
+    // Add request to queue
+    public void requestFloor(int floor) {
+        System.out.println("Request received for floor: " + floor);
+        requests.add(floor);
+        processRequests();
+    }
+
+    // Process queue
+    private void processRequests() {
+        while (!requests.isEmpty()) {
+            int targetFloor = requests.poll();
+            currentState.handleRequest(this, targetFloor);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         Elevator elevator = new Elevator();
 
         elevator.requestFloor(5);
         elevator.requestFloor(2);
-        elevator.requestFloor(2);
+        elevator.requestFloor(7);
+        elevator.requestFloor(7); // same floor test
     }
 }
