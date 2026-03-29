573ebe6bbd6a12b0f22f1499666c032c65fe734c
Sapna Singh <sapnasinghh71@gmail.com>
2026-03-29 21:43:55 +0530

    Implemented State interface

--- src/State.java
+++ src/State.java
@@ -1,3 +1,8 @@
 public interface State {
+    /**
+     * Handles the floor request based on the current state of the elevator.
+     * @param elevator The context object (the elevator itself).
+     * @param floor The destination floor requested by the user.
+     */
     void handleRequest(Elevator elevator, int floor);
 }
