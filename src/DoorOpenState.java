4ff899b964eceb0d013082a725a50e0861e64443
Sapna <sapnasinghh71@gmail.com>
2026-03-29 13:55:27 +0530

    Documented MovingUpState behavior

--- src/DoorOpenState.java
+++ src/DoorOpenState.java
@@ -1,3 +1,4 @@
+// Opens door after reaching destination
 public class DoorOpenState implements State {
     public void handleRequest(Elevator elevator, int floor) {
         System.out.println("Door opening at floor " + elevator.getCurrentFloor());
