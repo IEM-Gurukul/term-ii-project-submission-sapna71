d5038b590474288bbe47aea3ef52dcaeeb654bd1
Sapna <sapnasinghh71@gmail.com>
2026-03-29 13:57:05 +0530

    Implemented DoorOpenState

--- src/DoorOpenState.java
+++ src/DoorOpenState.java
@@ -1,4 +1,3 @@
-// Opens door after reaching destination
 public class DoorOpenState implements State {
     public void handleRequest(Elevator elevator, int floor) {
         System.out.println("Door opening at floor " + elevator.getCurrentFloor());
