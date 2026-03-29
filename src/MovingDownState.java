ad8aa136f6a70453b55ff2fe6d8af116c6d0d901
Sapna Singh <sapnasinghh71@gmail.com>
2026-03-29 21:48:16 +0530

    Updated & finalized MovingDownState

--- src/MovingDownState.java
+++ src/MovingDownState.java
@@ -2,6 +2,7 @@ public class MovingDownState implements State {
     public void handleRequest(Elevator elevator, int floor) {
         System.out.println("Moving down to floor " + floor);
         elevator.setCurrentFloor(floor);
+        // Transition to DoorOpenState upon arrival
         elevator.setState(new DoorOpenState());
         elevator.requestFloor(floor);
     }
