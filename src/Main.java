83d9bcb4fc2b3554eb463f68ef77e41c8d69731d
Sapna Singh <sapnasinghh71@gmail.com>
2026-03-29 21:50:58 +0530

    Implemented queue for handling multiple requests

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Elevator elevator = new Elevator();
-
+//requests
         elevator.requestFloor(5);
         elevator.requestFloor(2);
         elevator.requestFloor(7);
